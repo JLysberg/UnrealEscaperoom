0b3f61236fa238f738312468204d854c0a7c44f5
JLysberg <jostein.lysberg@gmail.com>
2019-07-21 01:27:33 +0200

    Finish testing positionreport

--- EscapeRoom/Source/EscapeRoom/PositionReport.cpp
+++ EscapeRoom/Source/EscapeRoom/PositionReport.cpp
@@ -3,6 +3,8 @@
 
 #include "PositionReport.h"
 
+#include "Classes/GameFramework/Actor.h"
+
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
 {
@@ -19,7 +21,8 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for %s"), *ObjectName);
 	
 }
 
